e3e22dd79fdca98e2cae7b089126b270ae308032
hamstar <webutek@gmail.com>
2017-08-23 00:11:21 -0700

    v1.0.2 * Reduced spawn rate

--- ConfigDefaults.cs
+++ ConfigDefaults.cs
@@ -3,14 +3,14 @@
 
 namespace Psycho {
 	public class ConfigurationData {
-		public readonly static Version ConfigVersion = new Version( 1, 0, 1 );
+		public readonly static Version ConfigVersion = new Version( 1, 0, 2 );
 
 
 		public string VersionSinceUpdate = "";
 
 		public bool Enabled = true;
 
-		public float PsychoSpawnChance = 0.04f;
+		public float PsychoSpawnChance = 0.015f;
 		public int PsychoHealRate = (int)(60f * 1.5f);
 		public int PsychoHealAmount = 50;
 		public bool PsychoCanDropLoot = true;
@@ -34,6 +34,11 @@ namespace Psycho {
 					this.PsychoSpawnChance = new_config.PsychoSpawnChance;
 				}
 			}
+			if( vers_since < new Version( 1, 0, 2 ) ) {
+				if( ConfigurationData._1_0_1_PsychoSpawnChance == this.PsychoSpawnChance ) {
+					this.PsychoSpawnChance = new_config.PsychoSpawnChance;
+				}
+			}
 
 			this.VersionSinceUpdate = ConfigurationData.ConfigVersion.ToString();
 
@@ -44,6 +49,7 @@ namespace Psycho {
 
 		public string _OLD_SETTINGS_BELOW = "";
 
+		public readonly static float _1_0_1_PsychoSpawnChance = 0.04f;
 		public readonly static float _1_0_0_PsychoSpawnChance = 0.02f;
 	}
 }
